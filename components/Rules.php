720f88f039d3d5fbc14cf5e0d1b854c5c6b3e627
Jin8-Nikolay <ramnage3@gmail.com>
2021-02-27 20:32:11 +0200

    Add project

---
+++ components/Rules.php
@@ -0,0 +1,6 @@
+<?php
+class Rules{
+    public static function  isAdmin(){
+
+    }
+}
---
+++ controllers/AdminProfileController.php
@@ -0,0 +1,26 @@
+<?php
+
+class AdminProfileController extends BaseController
+{
+    public function actionIndex()
+    {
+        $setting = new UserSetting();
+        if (isset($_POST['UserSetting'])) {
+            $data = $_POST['UserSetting'];
+            $userId = 1;
+            foreach ($data as $name => $params) {
+                if (!empty($params)) {
+                    if ($setting->find($userId, $name)) {
+                        $setting->update($userId, $name, $params);
+                    } else {
+                        $setting->add($userId, $name, $params);
+                    }
+                }
+            }
+        }
+        if (isset($data['save'])){
+            Redirect::run('/admin/profile');
+        }
+        return $this->renderAdmin('profile/index',['setting'=>$setting]);
+    }
+}//AdminProfileController
